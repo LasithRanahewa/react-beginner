753bc3bb3ba37980f524c64081c9bfa7a2b0efad
Lasith Madhuka <95028840+LasithM@users.noreply.github.com>
2023-01-14 12:12:55 +0530

    frontend architecture and folder structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div className="text-lg text-red-500">Hello</div>
+    <div className="text-lg text-red-500"><Home/></div>
   );
 }
 
