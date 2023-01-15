5363a2a3277f79cf5950dd5fac23ae5e6e3d7ab8
Lasith Madhuka <95028840+LasithM@users.noreply.github.com>
2023-01-15 13:52:31 +0530

    pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import Store from "./pages/Store";
+import Social from "./pages/Social";
+import Contact from "./pages/Contact";
+import Help from "./pages/Help";
 
 function App() {
   return (
-    <div><Home/></div>
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/store" element={<Store />} />
+          <Route exact path="/social" element={<Social />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/help" element={<Help />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -2,12 +2,12 @@ import { FaApple } from "react-icons/fa";
 import { FaSearch } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
 
-// import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-const NavbarItem = ({ name }) => {
+const NavbarItem = ({ path,name }) => {
   return (
     <p className="px-5 cursor-pointer hover:text-gray-500 transition-all ease-in-out">
-      {name}
+      <NavLink to={path}>{name}</NavLink>
     </p>
   );
 };
@@ -17,20 +17,15 @@ function Navbar() {
     <div className="p-4 bg-gray-800 sticky flex flex-row gap-3 justify-between items-center text-gray-100 shadow-md">
       <FaApple />
       <div className="flex flex-row gap-5">
-        <NavbarItem name="Store" />
-        <NavbarItem name="Social" />
-        <NavbarItem name="Contact" />
-        <NavbarItem name="Help" />
+        <NavbarItem path = "/store" name="Store" />
+        <NavbarItem path = "/social"  name="Social" />
+        <NavbarItem path = "/contact"  name="Contact" />
+        <NavbarItem path = "/help"  name="Help" />
       </div>
       <div className="flex flex-row gap-4">
         <FaSearch />
         <FaShoppingCart />
       </div>
-
-      {/* <NavLink to="/store">Store</NavLink>
-      <NavLink to="/social">Social</NavLink>
-      <NavLink to="/contact">Contact</NavLink>
-      <NavLink to="/help">Help</NavLink> */}
     </div>
   );
 }
