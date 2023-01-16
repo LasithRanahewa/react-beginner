4accbf51cef53b8b11cf08e6ca1d8e2bfd79732d
Lasith Madhuka <95028840+LasithM@users.noreply.github.com>
2023-01-17 04:33:37 +0530

    banner modification

--- src/components/banners/MainBanner.js
+++ src/components/banners/MainBanner.js
@@ -6,7 +6,7 @@ function MainBanner() {
     <div className="flex flex-col gap-3 items-center justify-center py-3">
       <p className="text-[40px] font-semibold">iPhone 14</p>
       <p className="font-thin text-lg">With awesome quality and performance</p>
-      <div className="flex flex-row gap-1">
+      <div className="flex flex-row gap-1 py-3">
         <StandardLink path="/iphone/iphone-14" name="Learn more >" />
         <StandardLink path="/iphone/buy/iphone-14" name="Buy >" />
       </div>
