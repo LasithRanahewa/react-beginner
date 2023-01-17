0ca024afeb3469cc67bf0555aeec14cdaa797074
Lasith Madhuka <95028840+LasithM@users.noreply.github.com>
2023-01-17 23:16:25 +0530

    Container component and Dynamic rendering using map

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -4,7 +4,7 @@ import { FaShoppingCart } from "react-icons/fa";
 
 import { NavLink } from "react-router-dom";
 
-const NavbarItem = ({ path,name }) => {
+const NavbarItem = ({ path, name }) => {
   return (
     <p className="px-5 cursor-pointer hover:text-gray-500 transition-all ease-in-out">
       <NavLink to={path}>{name}</NavLink>
@@ -17,10 +17,10 @@ function Navbar() {
     <div className="p-4 bg-gray-800 sticky flex flex-row gap-3 justify-between items-center text-gray-100 shadow-md">
       <FaApple />
       <div className="flex flex-row gap-5">
-        <NavbarItem path = "/store" name="Store" />
-        <NavbarItem path = "/social"  name="Social" />
-        <NavbarItem path = "/contact"  name="Contact" />
-        <NavbarItem path = "/help"  name="Help" />
+        <NavbarItem path="/store" name="Store" />
+        <NavbarItem path="/social" name="Social" />
+        <NavbarItem path="/contact" name="Contact" />
+        <NavbarItem path="/help" name="Help" />
       </div>
       <div className="flex flex-row gap-4">
         <FaSearch />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,53 @@
 import StandardLayout from "../components/layout/StandardLayout";
+import StandardContainer from "../components/containers/StandardContainer";
 
 import MainBanner from "../components/banners/MainBanner";
+
+//images
+import img1 from "../images/iphone-se.jpg";
+import img2 from "../images/airpods.jpeg";
+import img3 from "../images/ipad.jpg";
+import img4 from "../images/iwatch.jpg";
+
+const DATA = [
+  {
+    title: "iPhone SE",
+    description: "With awesome quality and performance",
+    image: img1,
+    style: "bg-gray-100",
+  },
+  {
+    title: "Airpods",
+    description: "With awesome quality and performance",
+    image: img2,
+  },
+  {
+    title: "iPad",
+    description: "With awesome quality and performance",
+    image: img3,
+  },
+  {
+    title: "iWatch",
+    description: "With awesome quality and performance",
+    image: img4,
+    style: "bg-gray-100",
+  },
+];
 
 function Home() {
   return (
     <StandardLayout>
       <MainBanner />
+      <div className="grid grid-cols-2">
+        {DATA.map((item) => (
+          <StandardContainer
+            title={item.title}
+            description={item.description}
+            image={item.image}
+            style={item.style}
+          />
+        ))}
+      </div>
     </StandardLayout>
   );
 }
